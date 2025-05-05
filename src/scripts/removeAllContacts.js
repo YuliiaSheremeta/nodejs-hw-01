8af94a18129c318e5ed1a5893abc450cff66f92d
Yuliia_Sheremeta <sheremeyulya1996@gmail.com>
2025-05-05 20:47:40 +0200

    add second part of tasks

--- src/scripts/removeAllContacts.js
+++ src/scripts/removeAllContacts.js
@@ -1,3 +1,16 @@
-export const removeAllContacts = async () => {};
+import { PATH_DB } from '../constants/contacts.js';
+import * as fs from 'node:fs/promises';
+
+export const removeAllContacts = async () => {
+
+    const data = await fs.readFile(PATH_DB, 'utf-8');
+    const contacts = JSON.parse(data);
+
+    contacts.length = 0;
+
+    await fs.writeFile(PATH_DB, JSON.stringify(contacts, null, 2), 'utf-8');
+    return contacts;
+
+};
 
 removeAllContacts();
--- src/scripts/removeLastContact.js
+++ src/scripts/removeLastContact.js
@@ -1,3 +1,15 @@
-export const removeLastContact = async () => {};
+import { PATH_DB } from '../constants/contacts.js';
+import * as fs from 'node:fs/promises';
+
+export const removeLastContact = async () => {
+    const data = await fs.readFile(PATH_DB, 'utf-8');
+    const contacts = JSON.parse(data);
+
+    if (contacts.length > 1) {
+        contacts.pop();
+        await fs.writeFile(PATH_DB, JSON.stringify(contacts, null, 2), 'utf-8');
+    }
+    return contacts;
+};
 
 removeLastContact();
